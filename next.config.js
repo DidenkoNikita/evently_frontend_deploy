1da43d5cd39630183be74f5b82dedfb2799834e2
DidenkoNikita <n.didenko@redlight.group>
2023-08-25 17:59:44 +0300

    update server url

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: 'http://localhost:3000/',
+    API_URL: 'http://158.160.75.150:3000/',
     CITY_URL: 'http://api.geonames.org/searchJSON?country=RU&lang=en&username=nikita_didenko',
-    CONNECT: 'ws://localhost:3000/chat'
+    CONNECT: 'ws://http://158.160.75.150:3000/chat'
   },
   images: {
     domains: ['storage.yandexcloud.net'],
